e64dfa8cd1c6c32338dc0394634d3715b89f194e
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-04 19:43:42 -0300

    02

--- src/ReactComponents/Calculator.jsx
+++ src/ReactComponents/Calculator.jsx
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from "react";
-import NumberBox from "./Number_box";
+//import NumberBox from "./Number_box";
+import NumericB from "./numeric_b";
 
 export default function CalculatorBox(){
-
+    
     return(
         <div id="Calculator">
-            <NumberBox id="Number_box"/>
+            <NumericB/>
         </div>
     );
 }
