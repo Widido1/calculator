7cde2be2d9b5360574cd955664d1019f46a2feb4
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-04 19:57:02 -0300

    ss

--- src/ReactComponents/Calculator.jsx
+++ src/ReactComponents/Calculator.jsx
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from "react";
 //import NumberBox from "./Number_box";
-import NumericB from "./numeric_b";
+import NumericB from ".\Numeric_b.jsx";
 
 export default function CalculatorBox(){
     
     return(
         <div id="Calculator">
-            <NumericB/>
+            <NumericB />
         </div>
     );
 }
--- src/ReactComponents/Numeric_b.jsx
+++ src/ReactComponents/Numeric_b.jsx
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from "react";
 
-export default function NumericB(n){
-    const [number, setnumber] = useState(n);
+export default function NumericB(){
+    const [number, setnumber] = useState();
 
     return (
         <div>    
-            <button class="b1">{n}</button>
+            <button class="b1">{}</button>
         </div>
     );
         
