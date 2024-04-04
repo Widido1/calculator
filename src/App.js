a3eb669741fb0ff62adbadf76907b73189df81e2
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-04 18:26:42 -0300

    try 02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Calculator from './ReactComponents/Calculator';
+import CalculatorBox from './ReactComponents/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <Calculator/>
+      <CalculatorBox/>
     </div>
   );
 }
--- src/ReactComponents/Calculator.jsx
+++ src/ReactComponents/Calculator.jsx
@@ -1,32 +1,11 @@
 import React, {useState, useEffect} from "react";
 import NumberBox from "./Number_box";
-import NumericB from "./Numeric_b";
 
-export default function Calculator(){
+export default function CalculatorBox(){
 
     return(
         <div id="Calculator">
-            <section>
-                <NumberBox/>
-            </section>
-            <section>
-                <NumericB value="7" />
-                <NumericB value="8" />
-                <NumericB value="9" />
-            </section>
-            <section>
-                <NumericB value="4" />
-                <NumericB value="5" />
-                <NumericB value="6" />
-            </section>
-            <section>
-                <NumericB value="1" />
-                <NumericB value="2" />
-                <NumericB value="3" />
-            </section>
-            <section>
-                <NumericB value="0" />
-            </section>
+            <NumberBox id="Number_box"/>
         </div>
     );
 }
