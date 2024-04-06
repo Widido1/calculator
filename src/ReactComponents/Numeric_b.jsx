fe83a7833b78f8686e06a7e53b98f9075fab138d
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-05 21:06:54 -0300

    try to change the css

--- src/ReactComponents/Numeric_b.jsx
+++ src/ReactComponents/Numeric_b.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from "react";
 export default function NumericB(){  
     return (
         <div>    
-            <button class="b1">{B1}</button>
+            <button class="b1">B1</button>
         </div>
     );
         
