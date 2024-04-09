44ede223440e60b2a0eaa1fbd44f91e812e595ce
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-09 14:44:56 -0300

    restore b1 css button property

--- src/ReactComponents/Numeric_b.jsx
+++ src/ReactComponents/Numeric_b.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 
 export default function NumericB(){  
     return (    
-        <button class="b1">B1</button>
+        <button>B1</button>
     );
         
 
