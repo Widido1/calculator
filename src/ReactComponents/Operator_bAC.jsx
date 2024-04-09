b613ec68f962b3081cb0bf21e5f2a62556600780
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-09 15:19:15 -0300

    add Operator_bAC

---
+++ src/ReactComponents/Operator_bAC.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react";
+
+export default function Operator_b(){
+    return(
+        <div>
+            <button class="bAC">O</button>
+        </div>
+    )
+}
