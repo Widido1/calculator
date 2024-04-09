70173c5a3703a8f2311c46277e27040c2489e027
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-09 17:54:33 -0300

    trying to fix Numeric_b

--- src/ReactComponents/Numeric_b.jsx
+++ src/ReactComponents/Numeric_b.jsx
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from "react";
 
 export default function NumericB(){  
+    const [bNumber, setbNumber] = useState(0);
+    const createbNumber = () => setbNumber(number);
+    
     return (    
-        <button class="b1">B1</button>
+        <button class="b1" onClick={addNumber(bNumber)}>B1</button>
     );
         
 
