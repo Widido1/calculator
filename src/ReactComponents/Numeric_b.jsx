899208aec0db7d7e312bf78167f0458723292a1c
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-09 17:55:28 -0300

    the same thig

--- src/ReactComponents/Numeric_b.jsx
+++ src/ReactComponents/Numeric_b.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 
 export default function NumericB(){  
     const [bNumber, setbNumber] = useState(0);
-    const createbNumber = () => setbNumber(number);
+    const createbNumber = (number) => setbNumber(number);
     
     return (    
         <button class="b1" onClick={addNumber(bNumber)}>B1</button>
