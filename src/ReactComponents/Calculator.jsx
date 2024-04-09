11b86f508f6a1ffb1019f25db2e8e0abdd2fdd66
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-09 14:49:08 -0300

    trying to create a different component for the 0

--- src/ReactComponents/Calculator.jsx
+++ src/ReactComponents/Calculator.jsx
@@ -21,7 +21,7 @@ export default function CalculatorBox(){
                 <Operator_b/>
             </div>
             <div id="Calculator0">
-                <NumericB id="cero_b"/>
+                <NumericB0/>
                 <Operator_b/>
             </div> 
 
---
+++ src/ReactComponents/numeric_b0.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react";
+
+export default function NumericB0(){  
+    return (    
+        <button class="b0">B1</button>
+    );
+        
+
+};
