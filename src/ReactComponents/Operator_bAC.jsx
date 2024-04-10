0ffa3c7c81c44b92fc4bb15f96a8dae607966bef
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-10 15:12:54 -0300

    className x2

--- src/ReactComponents/Operator_bAC.jsx
+++ src/ReactComponents/Operator_bAC.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from "react";
 export default function Operator_bAC(){
     return(
         <div>
-            <button class="bAC">O</button>
+            <button className="bAC">O</button>
         </div>
     )
 }
--- src/ReactComponents/Screen.jsx
+++ src/ReactComponents/Screen.jsx
@@ -4,8 +4,8 @@ export default function Screen(){
 
     return(
         <div id="CalculatorScreen">
-            <textbox id="Screen1" placeholder="0">hola</textbox>
-            <textbox id="Screen2" placeholder="0">123</textbox>
+            <div id="Screen1" placeholder="0">hola</div>
+            <div id="Screen2" placeholder="0">123</div>
         </div>
 
     );
