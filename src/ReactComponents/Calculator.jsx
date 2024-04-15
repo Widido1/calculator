a6be3c96bbd6f6839bcab404a1a3f74ab1f86bdd
Widido1 <157655060+Widido1@users.noreply.github.com>
2024-04-15 15:40:47 -0300

    add functionality to decimal button

--- src/ReactComponents/Calculator.jsx
+++ src/ReactComponents/Calculator.jsx
@@ -6,6 +6,7 @@ import Operator_b from "./Operator_b";
 import Operator_bAC from "./Operator_bAC";
 import Screen from "./Screen";
 import Result_b from "./Result_b";
+import Decimal_b from "./Decimal_b";
 
 export default function CalculatorBox(){
     const [N1, setN1] = useState(0);
@@ -13,11 +14,16 @@ export default function CalculatorBox(){
     const [N2, setN2] = useState(0);
     const [R, setR] = useState(0);
     const [targetN, setTargetN] = useState("N1");
+    const [isDecimal, setIsDecimal] = useState(false);
+    const [decimal, setDecimal] = useState([0, 0.1]);
 
     const clearAc = () => {
         setR(0); setN1(0); setN2(0); setOp("default_0"); setTargetN("N1");
         console.clear();
         console.log("Cleared Calculator!");
+    }
+    const decimaler = () => {
+        setIsDecimal(true);
     }
     const result = () =>{
         if(Op == "+"){
@@ -31,48 +37,70 @@ export default function CalculatorBox(){
         }else if(Op == "default_O"){
             setR(N1);
         }
-        setOp("default_O"); setTargetN("N1");
+        setOp("default_O"); setTargetN("N1"); setIsDecimal(false);
         console.log("The result: "+ R + " the Operator: "+Op);
     }
 
     const addNumber = (number) => {
         if(targetN == "N1"){
-            let resulter = N1*10 + number;
-            setN1(resulter);
-            console.log("Resulter: " + resulter + " N1: " + N1 +" Operator: " + Op);
-            setR(0);
+            if(isDecimal){
+                let des = (decimal[0]) + (number*decimal[1]);
+                let ndes = decimal[1] * 0.1; 
+                setDecimal([des, ndes]);
+            }else{
+                let resulter = N1*10 + number;
+                setN1(resulter);
+                console.log("Resulter: " + resulter + " N1: " + N1 +" Operator: " + Op);
+                setR(0);
+            }
+
         }else if(R != 0){
             setN1(R);
             setR(0);
-            let resulter = N2*10 + number;
-            setN2(resulter);
-            console.log("Resulter 2: " + resulter + " N2: " + N2 +" Operator: " + Op);
+            if(isDecimal){
+                let des = (decimal[0]) + (number*decimal[1]);
+                let ndes = decimal[1] * 0.1; 
+                setDecimal([des, ndes]);
+            }else{
+                let resulter = N2*10 + number;
+                setN2(resulter);
+                console.log("Resulter 2: " + resulter + " N2: " + N2 +" Operator: " + Op);
+            }        
         }else{
             setR(0);
-            let resulter = N2*10 + number;
-            setN2(resulter);
-            console.log("Resulter 2: " + resulter + " N2: " + N2 +" Operator: " + Op);
+            if(isDecimal){
+                let des = (decimal[0]) + (number*decimal[1]);
+                let ndes = decimal[1] * 0.1; 
+                setDecimal([des, ndes]);
+            }else{
+                let resulter = N2*10 + number;
+                setN2(resulter);
+                console.log("Resulter 2: " + resulter + " N2: " + N2 +" Operator: " + Op);
+            }
+
         }
         
     };
     const addResult = () =>{
         if(Op == "+"){
-            let res = N1 + N2;
+            let res = N1 + (N2+decimal[0]);
             setR(res); console.log("Result: " + res);
         }else if(Op == "-"){
-            let res = N1 - N2;
+            let res = N1 - (N2+decimal[0]);
             setR(res); console.log("Result: " + res);
         }else if(Op == "*"){
-            let res = N1*N2;
+            let res = N1*(N2+decimal[0]);
             setR(res); console.log("Result: " + res);
         }else if(Op == "/"){
-            let res = N1/N2;
+            let res = N1/(N2+decimal[0]);
             setR(res); console.log("Result: " + res);
 
         }
     }
     const addOperator = (button_O) =>{
+        setIsDecimal(false);
         if(targetN == "N1"){
+            setN1(N1 + decimal[0]); setDecimal([0,0.1]);
             switch(button_O){
                 case "+":
                     setOp("+"); setTargetN("N2"); console.log("switch: Operator: +"); 
@@ -93,7 +121,7 @@ export default function CalculatorBox(){
             console.log("llegamos a addResult()");
             addResult();
             console.log(" Operator: "+ Op);
-            setN1(0); setN2(0); setOp(button_O); console.log(" Operator: "+ Op);
+            setN1(0); setN2(0); setOp(button_O); setDecimal([0,0.1]); console.log(" Operator: "+ Op);
         }
     }
     return(
@@ -122,7 +150,7 @@ export default function CalculatorBox(){
             </div>
             <div id="Calculator0">
                 <NumericB0 addNumber={addNumber}/>
-                <Operator_b value={"."}/>
+                <Decimal_b decimaler={decimaler}/>
                 <Operator_b addOperator={addOperator} value={"/"}/>
             </div> 
 
---
+++ src/ReactComponents/Decimal_b.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from "react";
+
+export default function Decimal_b(props){
+
+    return(
+        <div>
+            <button className="b2" onClick={() => props.decimaler(true)}>.</button>
+        </div>
+    )
+}
